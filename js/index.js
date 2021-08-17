905d3641a875075f86ea3a65c8826f2d55d41492
slawless08 <samuel.l.lawless@gmail.com>
2021-08-17 13:29:50 -0400

    Creating intial js files to run inquirer.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const db_username = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const db_name = process.env.DB_NAME;
+
+const db = mysql.createConnection(
+    {
+    host: 'localhost',
+    user: db_username,
+    password: db_password,
+    database: db_name
+    },
+    console.log(`Connected to database: ${db_name}`)
+);
