3fb1f234893cda750aa4157e59a2e0b7d45afccc
slawless08 <samuel.l.lawless@gmail.com>
2021-08-23 11:32:18 -0400

    Changed organization and added code to view all parts.

--- js/view_data.js
+++ js/view_data.js
@@ -1,52 +1,67 @@
-const mysql = require('mysql');
-const db_username = process.env.DB_USER;
-const db_password = process.env.DB_PASSWORD;
-const db_name = process.env.DB_NAME;
-
-const db = mysql.createConnection(
-    {
-    host: 'localhost',
-    user: db_username,
-    password: db_password,
-    database: db_name
-    },
-);
-
-const cTable = require('console.table');
-
 const viewData = () => {
-    inquirer 
-    .prompt([
+
+    const inquirer = require("inquirer");
+
+    const mysql = require('mysql');
+    const db_username = process.env.DB_USER;
+    const db_password = process.env.DB_PASSWORD;
+    const db_name = process.env.DB_NAME;
+
+    const db = mysql.createConnection(
         {
-            type:'list',
-            name:'views',
-            message:'Which would you like to view?',
-            choices: [
-                'View employees', 'View departments', 'View roles',
-            ]
-        }
-    ]) .then ((answer) => {
-        switch (answer.views) {
-            case 'View employees':
-                viewEmployees();
-                break;
-            case 'View departments':
-                viewDepartments();
-                break;
-            case 'View roles':
-                viewRoles();
-                break;
-        }
-    })
-};
+            host: 'localhost',
+            user: db_username,
+            password: db_password,
+            database: db_name
+        },
+    );
 
-const viewEmployees = () => {
-    db.query("SELECT * FROM employee;", (err,results) => {
-        console.table(results);
-    });
-};
+    const cTable = require('console.table');
 
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'views',
+                message: 'Which would you like to view?',
+                choices: [
+                    'View employees', 'View departments', 'View roles',
+                ]
+            }
+        ]).then((answer) => {
+            switch (answer.views) {
+                case 'View employees':
+                    viewEmployees();
+                    break;
+                case 'View departments':
+                    viewDepartments();
+                    break;
+                case 'View roles':
+                    viewRoles();
+                    break;
+            }
+        })
+
+
+    const viewEmployees = () => {
+        db.query("SELECT * FROM employee;", (err, results) => {
+            console.table(results);
+        });
+    };
+
+    const viewDepartments = () => {
+        db.query("SELECT * FROM departments;", (err, results) => {
+            console.table(results);
+        });
+    };
+
+    const viewRoles = () => {
+        db.query("SELECT * FROM role;", (err, results) => {
+            console.table(results);
+        });
+    };
+
+};
 
 module.exports = viewData;
-module.exports = viewEmployees;
 
